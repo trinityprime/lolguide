d21e02dc4f82893ca5237d9e70038a2bf9d651c3
trinityprime <limkl.ryan@gmail.com>
2023-05-17 15:38:52 +0800

    html css js project

--- Assignment1HTML/lolguide.js
+++ Assignment1HTML/lolguide.js
@@ -5,3 +5,21 @@ window.addEventListener('scroll', function() {
   let scrollPercentage = (scrollPosition / (documentHeight - windowHeight)) * 100;
   document.querySelector('#scroll-indicator-progress').style.width = scrollPercentage + '%';
 });
+
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(tab => {
+  const input = tab.querySelector('input[type="radio"]');
+  const content = tab.querySelector('.tab-content');
+
+  input.addEventListener('change', () => {
+    tabs.forEach(otherTab => {
+      const otherContent = otherTab.querySelector('.tab-content');
+      if (otherTab !== tab && otherContent.style.display === 'block') {
+        otherContent.style.display = 'none';
+      }
+    });
+
+    content.style.display = 'block';
+  });
+});
