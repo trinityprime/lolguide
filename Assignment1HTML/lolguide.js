a9403169cbf0261358c0e748e2cf04d54cd91517
trinityprime <limkl.ryan@gmail.com>
2023-07-06 13:01:21 +0800

    accordian tabs need fix

--- Assignment1HTML/lolguide.js
+++ Assignment1HTML/lolguide.js
@@ -6,6 +6,28 @@ window.addEventListener('scroll', function () {
   document.querySelector('#scroll-indicator-progress').style.width = scrollPercentage + '%';
 });
 
+var lastScrollTop; // This Varibale will store the top position
+
+navbar = document.getElementById('sidebar'); // Get The NavBar
+
+window.addEventListener('scroll',function(){
+ //on every scroll this funtion will be called
+  
+  var scrollTop = document.documentElement.scrollTop;
+  //This line will get the location on scroll
+  
+  if(scrollTop > lastScrollTop){ //if it will be greater than the previous
+    navbar.style.top='-80px';
+    //set the value to the negetive of height of navbar 
+  }
+  
+  else{
+    navbar.style.top='0';
+  }
+  
+  lastScrollTop = scrollTop; //New Position Stored
+});
+
 const tabs = document.querySelectorAll('.tab');
 const tabContents = document.querySelectorAll('.tab-content');
 
@@ -30,6 +52,17 @@ tabs.forEach((tab) => {
   });
 });
 
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-
-
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
