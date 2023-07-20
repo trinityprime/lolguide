f3f0dda4619c1e4804a424743ad503d236bce6c4
trinityprime <limkl.ryan@gmail.com>
2023-07-20 22:20:18 +0800

    clean up

--- FORLOOPWHILELOOP_PRAC/Exercise4.py
+++
@@ -1,5 +0,0 @@
-#Write a program to print multiplication table of a given number
-
-num = 2
-for num in range (num, num*11, num):
-    print(num)
--- FORLOOPWHILELOOP_PRAC/Exercise9.py
+++
@@ -1,4 +0,0 @@
-#Display numbers from -10 to -1 using for loop
-
-for num in range(-10, 0, 1):
-    print(num)
